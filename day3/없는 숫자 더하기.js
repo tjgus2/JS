ed196284d085e51a294ee833d8a5238550248441
tjgus2 <162402315+tjgus2@users.noreply.github.com>
2025-06-10 22:29:07 +0900

    day3

---
+++ day3/없는 숫자 더하기.js
@@ -0,0 +1,9 @@
+// 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return 45-sum;
+}
